3ce870f4f9072c1f54c796227686714abe393241
abhishek singh <44000178+iabhishek15@users.noreply.github.com>
2020-05-30 13:44:52 +0530

    Create recurrence-of-divison.cpp

---
+++ recurrence-of-divison.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+struct __{__(){ios_base::Init i;ios_base::sync_with_stdio(0);cin.tie(0);}}__; 
+ 
+ 
+ int main() {
+    int ans = 0;
+    int val = 0;
+	for (int i = 2; i < 1000; ++i) {
+		int p = 10;
+		if (i > 10 && i <= 100) p = 100;
+		if (i > 100) p = 1000;
+		map<int,bool> m;
+		int cnt = 0;
+		while (1) {
+			int z = p % i;
+			if (m[p % i]) {
+				break;
+			}
+			m[p %  i] = 1;
+			p = 10 * z;
+		    if (z == 0) {
+				cnt++;
+				break;
+			}
+			if (p < i) {
+				p = 10 * p;
+				cnt++;
+			}
+			cnt++;
+		}
+		if (cnt > ans) {
+			ans = cnt;
+			val = i;
+		}
+	}
+	cout << val << " " << ans <<   endl;
+ }
+ 
+ 
+ 
+ 
+ 
