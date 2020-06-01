603a063787e7e92d7b5546fb10ee9c552772f882
abhishek singh <44000178+iabhishek15@users.noreply.github.com>
2020-06-02 00:41:43 +0530

    Update Dijkstra-for-directed-graph.cpp

--- Dijkstra-for-directed-graph.cpp
+++ Dijkstra-for-directed-graph.cpp
@@ -16,12 +16,12 @@ int main() {
 	vector<int> ans(n);
 	vector<bool> p(n);
 	set<pair<int,int>> s;
-	for (auto x : v[0]) {
+	for (auto x : v[0]) { //here we can replace 0 with any other vertex
 		s.insert({x.second, x.first});
 		ans[x.first] = x.second;
 		p[x.first] = 1;
 	}
-	p[0] = 1;
+	p[0] = 1; //here 0 should also be replaced
 	for (int i = 0; i < n; ++i) {
 		if (!p[i]) {
 			s.insert({INF,i});
@@ -29,17 +29,19 @@ int main() {
 		}
 	}
 	ans[0] = 0;	 
+	//set will only contain edges and once removed can never be added again
 	while (!s.empty()) {
 		auto it = s.begin();
 		ans[it->second] = it->first;
 		for (auto x : v[it->second]) {
 			auto tt = s.find({ans[x.first],x.first});
+			//value should be changed every time 
 			if (ans[x.first] == INF && ans[it->second] == INF) 
 				ans[x.first] = INF; 
 			else 
 				ans[x.first] = min(ans[x.first],ans[it->second] + x.second);
-			if (tt != s.end()) {
-				s.erase(tt);
+			if (tt != s.end()) { //we will check wether we have this edge in set or not
+				s.erase(tt); //if the edge is present will erase it and update the new one
 				s.insert({ans[x.first],x.first});
 			}
 		}	
