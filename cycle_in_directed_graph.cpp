0e7ab7e15747b1abeb85e83344e86963bbc76cfb
abhishek singh <44000178+iabhishek15@users.noreply.github.com>
2020-06-27 15:17:11 +0530

    Update cycle_in_directed_graph.cpp

--- cycle_in_directed_graph.cpp
+++ cycle_in_directed_graph.cpp
@@ -17,3 +17,39 @@ bool dfs(int index, vector<vector<int>> & v, vector<bool> &visited, vector<bool>
         rec[index] = 1;
         return 1;
     }
+        int p = 1e5 + 7;
+        vector<bool> visited(p);
+        vector<bool> rec(p);
+        vector<vector<int>> v(p);
+
+        for (int i = 0; i < n; ++i) {
+            v[pre[i][1]].push_back(pre[i][0]);
+        }
+        for (int i = 0; i < c; ++i) {
+            if (!rec[i] && !dfs(i,v,visited,rec)) {
+                return 0;
+            }
+        }
+        return 1;
+
+//it have to test whether the below one is true or not
+
+bool dfs(int index, vector<vector<int>> & v, vector<bool> &visited, vector<bool> &rec) {
+        if (rec[index]) {
+            return 1;
+        }
+        if (visited[index]) {
+            return 0;
+        }
+        visited[index] = 1;
+        for (auto x : v[index]) {
+            bool a  = dfs(x,v,visited,rec);
+            if (a) {
+               //rec[index] = 1;//here we can use this or nott depending accoding to the requirment if  we need the point where cycle is 
+                    //formed then we may need it
+               return 1;
+            }
+        }
+        rec[index] = 1;
+        return 0;
+    }
