f375cd8da5c5086c9980f3c9a53b589187162323
abhishek singh <44000178+iabhishek15@users.noreply.github.com>
2020-06-27 15:35:27 +0530

    Update cycle_in_directed_graph.cpp

--- cycle_in_directed_graph.cpp
+++ cycle_in_directed_graph.cpp
@@ -1,24 +1,24 @@
-//we should iterate over all the elements and check if anyone contains a cycle
-//there may be chances that 3 is in cycle with 4 then while traversing across 3 we may not
-//get cycle but while travering across 4 we will deinitely will get the cycle
 
-//here when we return 1 we mean false and when we return 0 we mean true
-
-bool dfs(int index, vector<vector<int>> & v, vector<bool> &visited, vector<bool> &rec) {
+:
+    bool isCycle(int index, vector<vector<int>> & v, vector<bool> &visited, vector<bool> &rec) {
         if (rec[index]) {
-            return 1;
+            return 0;
         }
         if (visited[index]) {
-            return 0;
+            return 1;
         }
         visited[index] = 1;
         for (auto x : v[index]) {
-            bool a  = dfs(x,v,visited,rec);
-            if (!a) return 0;
+            bool a  = isCycle(x,v,visited,rec);
+            if (a) {
+                return 1;
+            }
         }
         rec[index] = 1;
-        return 1;
+        return 0;
     }
+        
+  int main() {
         int p = 1e5 + 7;
         vector<bool> visited(p);
         vector<bool> rec(p);
@@ -28,30 +28,9 @@ bool dfs(int index, vector<vector<int>> & v, vector<bool> &visited, vector<bool>
             v[pre[i][1]].push_back(pre[i][0]);
         }
         for (int i = 0; i < c; ++i) {
-            if (!rec[i] && !dfs(i,v,visited,rec)) {
+            if (!rec[i] && isCycle(i,v,visited,rec)) {
                 return 0;
             }
         }
         return 1;
-
-//it have to test whether the below one is true or not
-
-bool dfs(int index, vector<vector<int>> & v, vector<bool> &visited, vector<bool> &rec) {
-        if (rec[index]) {
-            return 1;
-        }
-        if (visited[index]) {
-            return 0;
-        }
-        visited[index] = 1;
-        for (auto x : v[index]) {
-            bool a  = dfs(x,v,visited,rec);
-            if (a) {
-               //rec[index] = 1;//here we can use this or nott depending accoding to the requirment if  we need the point where cycle is 
-                    //formed then we may need it
-               return 1;
-            }
-        }
-        rec[index] = 1;
-        return 0;
-    }
+  }
