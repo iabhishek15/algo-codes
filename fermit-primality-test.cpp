0807b054c0f0ff11125d38410d327e3c86d6e958
abhishek singh <44000178+iabhishek15@users.noreply.github.com>
2020-07-09 17:17:45 +0530

    Update fermit-primality-test.cpp

--- fermit-primality-test.cpp
+++ fermit-primality-test.cpp
@@ -10,11 +10,12 @@ ll power_ch(ll a, ll b, ll mo) {
 	return ans;
 }         
 int main() {
+	//by this we can only proof than the number is composite we cannot say about the prime
 	ll n;
 	cin >> n;
 	ll x = 0;
-	for (ll i = 2; i < 22; ++i) {
-		if (i % n != 0) {
+	for (ll i = 2; i < 22; ++i) { //22  is used since we can go more than 21 to found a non divisor of number
+		if (i % n != 0) {   //since 21! is more than 2^18 , so we will get non divisor quickly
 			x = i;
 			break;
 		}
