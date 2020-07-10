f4fea4d5d5b02373102cc89621c1c308cb234d6f
abhishek singh <44000178+iabhishek15@users.noreply.github.com>
2020-07-10 20:31:19 +0530

    Update euler-totient-function.cpp

--- euler-totient-function.cpp
+++ euler-totient-function.cpp
@@ -1,11 +1,14 @@
+// We can avoid floating point calculations in above method.The idea is to count all prime factors and their multiples and 
+// subtract this count from n to get the totient function value (Prime factors and multiples of prime factors won’t have gcd as 1)
+
 //1) Initialize result as n//
 //2) Consider every number 'p' (where 'p' varies from 2 to ?n). 
-  If p divides n, then do following
-   a) Subtract all multiples of p from 1 to n [all multiples of p
-      will have gcd more than 1 (at least p) with n]
-   b) Update n by repeatedly dividing it by p.
-3) If the reduced n is more than 1, then remove all multiples
-   of n from result.
+//   If p divides n, then do following
+//    a) Subtract all multiples of p from 1 to n [all multiples of p
+//       will have gcd more than 1 (at least p) with n]
+//    b) Update n by repeatedly dividing it by p.
+// 3) If the reduced n is more than 1, then remove all multiples
+//    of n from result.
 
 for (ll i = 2; i * i <= n; ++i){
 	if (n % i == 0) {
