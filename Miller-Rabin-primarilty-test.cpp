4e6125040e0c27a5cb75a044db2a5f0124e0c62b
abhishek singh <44000178+iabhishek15@users.noreply.github.com>
2020-08-01 18:24:20 +0530

    Update Miller-Rabin-primarilty-test.cpp

--- Miller-Rabin-primarilty-test.cpp
+++ Miller-Rabin-primarilty-test.cpp
@@ -6,8 +6,8 @@ const int k = 100;
 ll modmul(ll a, ll b, ll mod){
     ll res = 0;
     a %= mod;
-    while(b > 0){
-        if(b & 1)
+    while (b > 0){
+        if (b & 1)
             res = (res + a) % mod;
         a = (2 * a) % mod;
         b /= 2LL;
@@ -17,8 +17,8 @@ ll modmul(ll a, ll b, ll mod){
 ll mpow(ll base, ll exp, ll mod) {
     ll res = 1;
     base %= mod;
-    while(exp > 0){
-        if(exp & 1)
+    while (exp > 0){
+        if (exp & 1)
             res = modmul(res, base, mod);
         base = modmul(base, base, mod);
         exp /= 2LL;
@@ -28,29 +28,29 @@ ll mpow(ll base, ll exp, ll mod) {
 bool millerTest(ll n,ll d) {
     ll a = 2 + rand() % (n - 4);
     ll x = mpow(a, d, n);
-    if(x == 1 || x == n - 1)
+    if (x == 1 || x == n - 1)
         return true;
-    while(d != n - 1){
+    while (d != n - 1){
         x = modmul(x, x, n);
         d *= 2LL;
-        if(x == 1)
+        if (x == 1)
             return false;
-        if(x == n - 1)
+        if (x == n - 1)
             return true;
     }
     return false;
 }
 bool is_prime(ll n) {
-    if(n < 2)
+    if (n < 2)
         return false;
-    if(n < 4)
+    if (n < 4)
         return true;
-    if(n % 2LL == 0)
+    if (n % 2LL == 0)
         return false;
     ll d = n - 1;
-    while(d % 2LL == 0)
+    while (d % 2LL == 0)
         d /= 2LL;
-    for(int i = 0; i < k; i++) {
+    for (int i = 0; i < k; i++) {
         if(!millerTest(n,d))
             return false;
     }
@@ -62,7 +62,7 @@ int main() {
     while(t--){
         ll n;
         cin >> n;
-        if(is_prime(n))
+        if (is_prime(n))
             cout << "YES\n";
         else
             cout << "NO\n";
