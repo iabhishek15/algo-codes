dae264c62fad7293b0144997d01c80a955d623ef
abhishek singh <44000178+iabhishek15@users.noreply.github.com>
2020-08-30 18:23:11 +0530

    Create Z-function.cpp

---
+++ Z-function.cpp
@@ -0,0 +1,47 @@
+void check_pattern() {
+	string a, b;
+	cin >> a >> b;
+	string s = b + "$" + a;
+	int n = (int)s.size();
+	int b_size = (int)b.size();
+	vector<int> ans;
+	vector<int> Z(n);
+	int L = -1, R = -1;
+	for (int i = b_size + 1; i < n; ++i) {
+		if (i > R) {
+			L = R = i;
+			while (R < n && s[R] == s[R - L]) {
+				R++;
+			}
+			Z[i] = R - L;
+			R--;
+		}else {
+			int k = i - L;
+      if (Z[k] < R - i + 1) {
+        Z[i] = Z[k];
+      }else {
+				L = i;
+				while (R < n && s[R - L] == s[R]) {
+						R++;
+				}
+				Z[i] = R - L; 
+				R--;
+      }
+		}
+	}	
+	for (int i = 0; i < n; ++i) {
+		if (Z[i] == b_size) {
+			ans.push_back(i - b_size);
+		}
+	}
+	int k = (int)ans.size();
+	if (k > 0) {
+		cout << k << "\n";
+		for (int i = 0; i < k; ++i) {
+			cout << ans[i] << " ";
+		}
+	}else {
+		cout << "Not Found";
+	}
+	cout << "\n\n";
+}
