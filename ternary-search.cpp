904793d71c1559a8db5e96b84e42e53cf1aa389c
abhishek singh <44000178+iabhishek15@users.noreply.github.com>
2021-01-15 18:56:49 +0530

    Update ternary-search.cpp

--- ternary-search.cpp
+++ ternary-search.cpp
@@ -46,5 +46,27 @@ while (l < r) {
 
 
 
+//for double
+//problem : https://www.codechef.com/problems/ICM2003
+
+double l = 0.0;
+double p = 3.1415926535897932384626433832795028841971693993751;
+double r = p / 2.0;
+for (int i = 0; i < 100; ++i) {
+  double m1 = l + (r - l) / 3.0;
+  double m2 = r - (r - l) / 3.0;
+  double x = ((m1 * m1 + b * m1 + c) / (sin(m1)));
+  double y = ((m2 * m2 + b * m2 + c) / (sin(m2)));
+  if (x > y) {
+    l = m1;
+  }else {
+    r = m2;
+  }
+}
+double ans = (l * l + b * l + c)  / sin(l);
+cout << fixed << setprecision(10) << ans  << endl;
+
+
+
 
 
