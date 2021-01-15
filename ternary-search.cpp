484d1fa342dab5dcc82ce36fce620784281e8e51
abhishek singh <44000178+iabhishek15@users.noreply.github.com>
2021-01-15 15:56:40 +0530

    Create ternary-search.cpp

---
+++ ternary-search.cpp
@@ -0,0 +1,25 @@
+problem : https://codeforces.com/problemset/problem/439/D
+
+for getting the minimum value
+
+int l = 1;
+int r = 1e9 + 7;
+while (l < r) {
+  int m1 = l + (r - l) / 3;
+  int m2 = r - (r - l) / 3;
+  int val1 = 0;
+  int val2 = 0;
+  for (int i = 0; i < n; ++i) {
+    val1 += max(m1 - a[i], 0LL);
+    val2 += max(m2 - a[i], 0LL);
+  }
+  for (int i = 0; i < m; ++i) {
+    val1 += max(b[i] - m1, 0LL);
+    val2 += max(b[i] - m2, 0LL);
+  }
+  if (val1 <= val2) {
+    r = m2 - 1;
+  }else {
+    l = m1 + 1;
+  }
+}
