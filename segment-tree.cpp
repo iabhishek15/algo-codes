c05589fedce6a14a875f55e1bea9e691a57fe463
abhishek singh <44000178+iabhishek15@users.noreply.github.com>
2021-01-26 13:52:48 +0530

    Update segment-tree.cpp

--- segment-tree.cpp
+++ segment-tree.cpp
@@ -1,77 +1,132 @@
 #include <bits/stdc++.h> 
 using namespace std;
-int pos = 1e4;
-int n;
-vector<int> v(pos);
-vector<int> a(pos);
 
-void build_segment_tree(int start, int end, int index) {
-	if (start == end) {
-		v[index] = a[start]; 
-		return ;
-	}
-	int mid = (start + end) / 2;
-	build_segment_tree(start, mid, index*2);
-	build_segment_tree(mid + 1,end, index*2 + 1);
-	
-	v[index] = v[index*2] + v[index*2 + 1];
+
+int const nax = 2e5 + 11;
+int n; 
+int v[nax];
+int seg[nax * 4];
+
+//for sum
+
+void build_sum(int start = 1, int end = n, int index = 1) {
+  if (start == end) {
+    seg[index] = v[start]; 
+    return ;
+  }
+  int mid = (start + end) / 2;
+  build_sum(start, mid, index * 2);
+  build_sum(mid + 1,end, index * 2 + 1);
+  
+  seg[index] = seg[index * 2] + seg[index * 2 + 1];
+}
+long long query_sum(int x, int y, int start = 1, long long end = n, long long index = 1) {
+  if ((start >= x) &&  (end <= y)) {
+    return seg[index];
+  }
+  if ((y < start) || (x > end)) {
+    return 0;
+  } 
+  long long mid = (start + end) / 2;
+  return query_sum(x, y, start, mid, index * 2) + query_sum(x, y, mid + 1, end, index * 2 + 1);
 }
 
-int query_segment(int x, int y, int start, int end, int index) {
-	if ((start >= x) &&  (end <= y)) {
-		return v[index];
-	}
-	if ((y < start) || (x > end)) {
-		return 0;
-	} 
-	int mid = (start + end) / 2;
-	return query_segment(x, y, start, mid, index * 2) + query_segment(x, y, mid + 1, end, index * 2 + 1);
-}
-
-void query_update(int given_index, int value, int start, int end, int index) {
-	v[index] += value;
-	if (start == end) {
-		return ;
-	}
-	int mid = (start + end) / 2;
-	if (given_index <= mid) {
-		query_update(given_index, value, start, mid, index * 2);
-	}else {
-		query_update(given_index, value, mid + 1, end, index * 2 + 1);
-	}
+void update_sum(long long given_index, long long value, long long start = 1, long long end = n, long long index = 1) {
+  if (start == end) {
+    seg[index] = value;
+    return ;
+  }
+  int mid = (start + end) / 2;
+  if (given_index <= mid) {
+    update_sum(given_index, value, start, mid, index * 2);
+  }else {
+    update_sum(given_index, value, mid + 1, end, index * 2 + 1);
+  }
+  seg[index] = seg[2 * index] + seg[2 * index + 1];
 }
 
 
+//for min
 
-int main() {
-	cin >> n;
-	for (int i = 0; i < n; ++i) {
-		cin >> a[i];
-	}
-	build_segment_tree(0, n - 1, 1);
-	int k;
-	cin >> k;
-	while (k--) {
-		int x;
-		cin >> x;
-		if (x == 2) {
-			int left, right;
-			cin >> left >> right;
-			left--;
-			right--;
-			cout << query_segment(left, right, 0, n - 1, 1) << endl;
-		}else {
-			int index;
-			int value;
-			cin >> index >> value;
-			index--;
-			int lo = value - a[index];
-			a[index] = value;
-			query_update(index, lo, 0, n - 1, 1);
-		}
-	}
+void build_min(int start = 1, int end = n, int index = 1) {
+  if (start == end) {
+    seg[index] = v[start]; 
+    return ;
+  }
+  int mid = (start + end) / 2;
+  build_min(start, mid, index * 2);
+  build_min(mid + 1,end, index * 2 + 1);
+  seg[index] = min(seg[index * 2], seg[index * 2 + 1]);
 }
 
+int query_min(int x, int y, int start = 1, int end = n, int index = 1) {
+  if ((start >= x) &&  (end <= y)) {
+    return seg[index];
+  }
+  if ((y < start) || (x > end)) {
+    return 1e18 + 17;
+  } 
+  int mid = (start + end) / 2;
+  return min(query_min(x, y, start, mid, index * 2), query_min(x, y, mid + 1, end, index * 2 + 1));
+}
+
+
+void update_min(int given_index, int value, int start = 1, int end = n, int index = 1) {
+  if (start == end) {
+    seg[index] = value;
+    return ;
+  }
+  int mid = (start + end) / 2;
+  if (given_index <= mid) {
+    update_min(given_index, value, start, mid, index * 2);
+  }else {
+    update_min(given_index, value, mid + 1, end, index * 2 + 1);
+  }
+  seg[index] = min(seg[index * 2], seg[index * 2 + 1]);
+}
+
+
+
+
+
+
+//for max
+void build_max(int start = 1, int end = n, int index = 1) {
+  if (start == end) {
+    seg[index] = v[start]; 
+    return ;
+  }
+  int mid = (start + end) / 2;
+  build_max(start, mid, index * 2);
+  build_max(mid + 1,end, index * 2 + 1);
+  
+  seg[index] = max(seg[index * 2], seg[index * 2 + 1]);
+}
+
+int query_max(int x, int y, int start = 1, int end = n, int index = 1) {
+  if ((start >= x) &&  (end <= y)) {
+    return seg[index];
+  }
+  if ((y < start) || (x > end)) {
+    return -1e18 - 7;
+  } 
+  int mid = (start + end) / 2;
+  return max(query_max(x, y, start, mid, index * 2), query_max(x, y, mid + 1, end, index * 2 + 1));
+}
+
+void update_max(int given_index, int value, int start = 1, int end = n, int index = 1) {
+  if (start == end) {
+    seg[index] = value;
+    return ;
+  }
+  int mid = (start + end) / 2;
+  if (given_index <= mid) {
+    update_min(given_index, value, start, mid, index * 2);
+  }else {
+    update_min(given_index, value, mid + 1, end, index * 2 + 1);
+  }
+  seg[index] = max(seg[index * 2], seg[index * 2 + 1]);
+}
 
 
 
