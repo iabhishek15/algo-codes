eb9204fbdc1a5d79e259f270249a10c0a82ebd44
abhishek singh <44000178+iabhishek15@users.noreply.github.com>
2021-06-11 17:47:12 +0530

    Update Miller-Rabin-primarilty-test.cpp

--- Miller-Rabin-primarilty-test.cpp
+++ Miller-Rabin-primarilty-test.cpp
@@ -1,70 +1,60 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
-const int k = 100;
 
-ll modmul(ll a, ll b, ll mod){
-    ll res = 0;
-    a %= mod;
-    while (b > 0){
-        if (b & 1)
-            res = (res + a) % mod;
-        a = (2 * a) % mod;
-        b /= 2LL;
-    }
-    return res % mod;
+uint64_t modmul(uint64_t a, uint64_t b, uint64_t mod){
+  uint64_t res = 0;
+  a %= mod;
+  while (b > 0){
+    if (b & 1)
+      res = (res + a) % mod;
+    a = (2 * a) % mod;
+    b /= 2LL;
+  }
+  return res % mod;
 }
-ll mpow(ll base, ll exp, ll mod) {
-    ll res = 1;
-    base %= mod;
-    while (exp > 0){
-        if (exp & 1)
-            res = modmul(res, base, mod);
-        base = modmul(base, base, mod);
-        exp /= 2LL;
-    }
-    return res % mod;
+
+uint64_t binpower(uint64_t base, uint64_t e, uint64_t mod) {
+  uint64_t result = 1;
+  base %= mod;
+  while (e) {
+    if (e & 1)
+      result = modmul(result, base, mod);
+    base = modmul(base, base, mod);
+    e >>= 1;
+  }
+  return result;
 }
-bool millerTest(ll n, ll d) {
-    ll a = 2 + rand() % (n - 4);
-    ll x = mpow(a, d, n);
-    if (x == 1 || x == n - 1)
-        return true;
-    while (d != n - 1){
-        x = modmul(x, x, n);
-        d *= 2LL;
-        if (x == 1)
-            return false;
-        if (x == n - 1)
-            return true;
-    }
+
+bool check_composite(uint64_t N, uint64_t A, uint64_t D, int R) {
+  uint64_t x = binpower(A, D, N);
+  if (x == 1 || x == N - 1) {
     return false;
-}
-bool is_prime(ll n) {
-    if (n < 2)
-        return false;
-    if (n < 4)
-        return true;
-    if (n % 2LL == 0)
-        return false;
-    ll d = n - 1;
-    while (d % 2LL == 0)
-        d /= 2LL;
-    for (int i = 0; i < k; i++) {
-        if(!millerTest(n, d))
-            return false;
+  }
+  for (int r = 1; r < R; r++) {
+    x = modmul(x, x, N);
+    if (x == N - 1) {
+      return false;
     }
-    return true;
-}
-int main() {
-    int t;
-    cin >> t;
-    while(t--){
-        ll n;
-        cin >> n;
-        if (is_prime(n))
-            cout << "YES\n";
-        else
-            cout << "NO\n";
+  }
+  return true;
+};
+
+
+bool MillerRabin(uint64_t N) {
+  if (N < 2) {
+    return false;
+  }
+  int64_t D = N - 1;
+  int R = 0;
+  while ((D & 1) == 0) {
+    D >>= 1;
+    R++;
+  }
+  for (uint64_t A : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {
+    if (N == A) {
+      return true;
     }
+    if (check_composite(N, A, D, R)) {
+      return false;
+    }
+  }
+  return true;
 }
