192b658466e8354a77463a9bfe44b3ff8a6b41b5
abhishek singh <44000178+iabhishek15@users.noreply.github.com>
2021-06-24 21:26:52 +0530

    Update max_flow.cpp

--- max_flow.cpp
+++ max_flow.cpp
@@ -11,28 +11,34 @@ int Max_Flow() {
     vector<int> visit(N + 1);
     queue<int> q;
     q.push(source);
-    int flow = 1e18 + 7;
     while ((int)q.size()) {
       int top = q.front();
       q.pop();
       visit[top] = 1;
       for (auto x : Node[top]) {
         if (x.second && !visit[x.first]) {
-          flow = min(flow, x.second);
           q.push(x.first);
           flow_dirn[x.first] = top;
         }
       }
     }
     if (flow_dirn.count(sink) == 0) break;
+    
+    int flow = 1e18 + 7;
     int curr = sink;
+    while (flow_dirn[curr]) {
+      int new_curr = flow_dirn[curr];
+      flow = min(flow, Node[new_curr][curr]);
+      curr = new_curr;
+    }
+    curr = sink;
     while (flow_dirn[curr]) {
       int new_curr = flow_dirn[curr];
       Node[new_curr][curr] -= flow;
       Node[curr][new_curr] += flow;
       curr = new_curr;
     }
-    max_flow += flow;
+    max_flow += flow; 
   }
   return max_flow;
 }
